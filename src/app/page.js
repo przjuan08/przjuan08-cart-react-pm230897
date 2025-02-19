f674289a82992cd055e7301194595998c3c1f6da
Juan José <jp2004321@gmail.com>
2025-02-19 14:16:25 -0600

    Ejercicio de ejemplo - guía 4

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+"use client" 
+
+import { useState } from 'react';
+import { Headers } from "../components/Header";
+import { ProductList } from "../components/ProductList";
+export default function Home() { const [allProducts, setAllProducts] = useState([]); const [total, setTotal] = useState(0); const [countProducts, setCountProducts] = useState(0);
+return (
+<>
+<Headers allProducts={allProducts} setAllProducts={setAllProducts} total={total} setTotal={setTotal} countProducts={countProducts} setCountProducts={setCountProducts}
+/>
+<ProductList
+allProducts={allProducts} setAllProducts={setAllProducts} total={total} setTotal={setTotal} countProducts={countProducts} setCountProducts={setCountProducts}
+/>
+</>
+);
+}
